2e7fd3f923a527fc7f1a39e9c3fcb3fd649f62de
Kristy Eley <korameley@gmail.com>
2022-02-18 22:23:13 +0100

    Updates to UI and types

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Save The Lynx Game
 
-## _About:_
-
-Save The Lynx Game is a relatively simple game created to learn
-the ReactJS framework along with brushing up on my Flask knowledge.
+Save The Lynx Game is a two-player board game created to raise awarenesss of and 
+depict the restoration of the Canadian lynx habitat. With this theme in mind,
+my goal was to learn the ReactJS framework along with brushing up on my Flask knowledge.
 During this years Advent Of Code, I enjoyed utilizing the flood-fill algorithm
 to complete several of the questions. That is also what enlightened me to
 include the algorithm in this project.
@@ -13,3 +12,14 @@ include the algorithm in this project.
 - React and Flask frameworks.
 - Utilization of the flood-fill algorithm.
 - Migrating a project from Javascript to Typescript.
+
+## _Game overview:_
+
+Beginning with two players, each will input and submit their name.
+Once started, the game board is displayed as a 10x10 grid of numbers aka plots of land.
+The player to go first is displayed at the top of the board. Turns are alternated between players.
+A turn consists of a player selecting an unselected square plot on the board. 
+By selecting this plot of land, the player chooses to restore the habitat with trees native to
+that area. All four-connected adjacent numbers equal to the original selected number will also be restored. 
+The area continues to be restored until none of the four-connected neighbors are of the same original number. 
+The first player to restore the habitat of the lynx wins.
--- save-the-lynx-client/src/App.tsx
+++ save-the-lynx-client/src/App.tsx
@@ -1,56 +1,132 @@
-import Header from './components/header';
-import BoardSpace from './components/board-space';
-import classNames from 'classnames';
+import Header from "./components/header";
+import BoardSpace from "./components/board-space";
+import classNames from "classnames";
 import React, { useState } from "react";
-import { IPlayer } from './interfaces';
-
+import { IPlayer } from "./interfaces";
+import PlayerInputPaw from "./components/player-input-paw";
+import LynxAnimal from "./components/lynx-animal";
 
 function App() {
   const [playerOne, setPlayerOne] = useState("");
   const [playerTwo, setPlayerTwo] = useState("");
 
   const [currentPlayer, setCurrentPlayer] = useState<IPlayer>({
-    name: '',
-    id: 0
+    name: "",
+    id: 0,
   });
   const [board, setBoard] = useState([]);
 
-  // 0: setup, 1: game-play, 2: victory, 3: defeat
   const [gameState, setGameState] = useState(0);
 
-  const gameMove = ((move: number[]) => {
-    fetch('/move', {
+  const gameMove = (move: number[]) => {
+    fetch("/move", {
       method: "POST",
       headers: {
-      'Content-Type' : 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(move)
-    }).then(res => res.json())
-    .then(data => {
-      setGameState(data.state);
-      if (!data.state.won) {
-        setCurrentPlayer(data.next);
-        setBoard(data.board);
-      } else {
-        setGameState(2);
-      }
-    });
-  });
+      body: JSON.stringify(move),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setGameState(data.state);
+        if (!data.state.won) {
+          setCurrentPlayer(data.next);
+          setBoard(data.board);
+        } else {
+          setGameState(2);
+        }
+      });
+  };
 
   const startGame = () => {
-    fetch('/start-game', {
+    fetch("/start-game", {
       method: "POST",
       headers: {
-      'Content-Type' : 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify([playerOne, playerTwo])
-    }).then(res => res.json())
-    .then(data => {
-      setBoard(data.board);
-      setGameState(1);
-      setCurrentPlayer(data.firstPlayer);
+      body: JSON.stringify([playerOne, playerTwo]),
     })
-  }
+      .then((res) => res.json())
+      .then((data) => {
+        setBoard(data.board);
+        setGameState(1);
+        setCurrentPlayer(data.firstPlayer);
+      });
+  };
+
+  const showInputPlayers = () => {
+    if (gameState < 1) {
+      return (
+        <div className="PlayerFormContainer">
+          <form className="PlayerForm" onSubmit={handleSubmit}>
+            <label>First Player:</label>
+            <div className="PlayerFormInputButton">
+              <input
+                type="text"
+                value={playerOne}
+                onChange={(e) => setPlayerOne(e.target.value)}
+              />
+              <button type="submit">
+                <PlayerInputPaw />
+              </button>
+            </div>
+          </form>
+          <form className="PlayerForm" onSubmit={handleSubmit}>
+            <label>Second Player:</label>
+            <div className="PlayerFormInputButton">
+              <input
+                type="text"
+                value={playerTwo}
+                onChange={(e) => setPlayerTwo(e.target.value)}
+              />
+              <button type="submit">
+                <PlayerInputPaw />
+              </button>
+            </div>
+          </form>
+        </div>
+      );
+    }
+  };
+
+  const showStartGame = () => {
+    if (gameState < 1) {
+      return (
+        <button className="StartGameButton" onClick={() => startGame()}>
+          Start game
+        </button>
+      );
+    }
+  };
+
+  const showGamePlay = () => {
+    if (gameState >= 1) {
+      return (
+        <div className="GamePlay">
+          <h2>The current player is {currentPlayer.name}.</h2>
+          <div className="BoardContainer">
+            <ul className="BoardUl">
+              {board.map((row: number[], index: number) =>
+                row.map((column: number, colIndex: number) => (
+                  <BoardSpace
+                    key={[index, colIndex]}
+                    className={classNames(
+                      "space-state",
+                      board[index][colIndex] === 0 && "space-state-seen",
+                      board[index][colIndex] === -1 && "space-state-found"
+                    )}
+                    searched={board[index][colIndex]}
+                    coordinateClicked={processCoordinateClick}
+                    coordinate={[index, colIndex]}
+                  />
+                ))
+              )}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+  };
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -58,53 +134,17 @@ function App() {
 
   const processCoordinateClick = (chosenSpace: number[]) => {
     gameMove(chosenSpace);
-  }
+  };
 
   return (
     <div className="App">
-      <h1><Header /></h1>
-      <form onSubmit={handleSubmit}>
-        <label>Enter first player's name:
-          <input 
-            type="text" 
-            value={playerOne}
-            onChange={(e) => setPlayerOne(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
-      </form>
-      <form onSubmit={handleSubmit}>
-        <label>Enter second player's name
-          <input 
-            type="text" 
-            value={playerTwo}
-            onChange={(e) => setPlayerTwo(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
-      </form>
-      <button onClick={() => startGame()}>
-        Start game
-      </button>
-      <p>Game state: {gameState}.</p>
-      <ul>
-        {board.map((row: number[], index: number) =>
-          row.map((column: number, colIndex: number) =>
-            <BoardSpace
-              key={[index, colIndex]}
-              className={classNames(
-                'space-state',
-                board[index][colIndex] === 0 && 'space-state-seen',
-                board[index][colIndex] === -1 && 'space-state-found'
-              )}
-              searched={board[index][colIndex]}
-              coordinateClicked={processCoordinateClick}
-              coordinate={[index, colIndex]}
-            />
-          )
-        )}
-      </ul>
-      <p>The current player is {currentPlayer.name}.</p>
+      <h1 className="AppHeader">
+        <Header />
+      </h1>
+      {showInputPlayers()}
+      <LynxAnimal />
+      {showStartGame()}
+      {showGamePlay()}
     </div>
   );
 }
